01ff9a51923c50b350e55a5249ec181a74c17f5b
Ana Beatriz <anabeatrizcarvalhoesmaile@gmail.com>
2026-02-10 10:58:28 -0300

    :sparkles: Implementação do Menu

---
+++ Menu.ts
@@ -0,0 +1,108 @@
+
+import { Input } from "./src/util/Input";
+import { Colors } from "./src/util/Colors";
+
+
+
+export function main(){
+
+
+    while(true){
+console.log(Colors.bg.black, Colors.fg.magenta,
+            "\n==================================================");
+console.log("||                GEEK.COM                        ||");
+console.log("====================================================");
+console.log("||         |1| Criar PRODUTO                      ||");
+console.log("||         |2| Listar todos os Produtos           ||");
+console.log("||         |3| Buscar produto por ID              ||");
+console.log("||         |4| Atualizar Produto                  ||");
+console.log("||         |5| Apagar Produto                     ||");
+console.log("||         |0| Sair                               ||");
+console.log("====================================================");
+console.log("                                                  ",
+Colors.reset);
+
+
+    console.log("Digite a opção desejada:")
+    let opcao = Input.questionInt("")
+
+    
+
+    switch(opcao){
+
+        case 1:
+            console.clear();
+            console.log("=======================================")
+            console.log("||          Criar Produto            ||")
+            console.log("=======================================")
+        
+            keyPress();
+        break;
+
+        case 2:
+            console.clear();
+            console.log("=======================================")
+            console.log("||       Listar todos os Produtos      ||")
+            console.log("=======================================")
+           
+            keyPress();
+
+        break;
+
+        case 3:
+            console.clear();
+            console.log("=======================================")
+            console.log("||           Buscar Produto        ||")
+            console.log("=======================================")
+            
+            keyPress();
+
+        break;
+        case 4:
+            console.clear();
+            console.log("=======================================")
+            console.log("||         Atualizar Produto        ||")
+            console.log("=======================================")
+            
+         
+
+        break;
+        case 5:
+            console.clear();
+            console.log("=======================================")
+            console.log("||          Apagar Produto          ||")
+            console.log("=======================================")
+            
+
+
+        break;
+
+        case 0:
+        sobre();
+        process.exit(0);
+        break;
+
+        }
+
+    }
+  
+}
+
+
+    function sobre(): void {
+    console.log(Colors.bg.black, Colors.fg.cyan, 
+             "\n*****************************************************");
+    console.log("PROJETO DESENVOLVIDO POR : Ana Beatriz");
+    console.log("Email - anabeatrizcarvalhoesmaile@gmail.com");
+    console.log("github.com/itsbya");
+    console.log("*****************************************************");
+}
+  
+
+    /* Função de pausa entre as opções do menu */
+    function keyPress(): void {
+    console.log(Colors.reset,"\nPressione enter para continuar...");
+    Input.prompt();
+}
+
+main();
